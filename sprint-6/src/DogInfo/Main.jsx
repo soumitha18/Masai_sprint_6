7a40a02ed2799999cb8d631caf4dd5b9469e6131
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-08-12 11:15:59 +0530

    Created Files

--- sprint-6/src/DogInfo/Main.jsx
+++ sprint-6/src/DogInfo/Main.jsx
@@ -1,10 +1,40 @@
 import React from "react"
+import Home from "./Home"
+import Dog from "./Dog"
+import Account from "./Account"
 
 export default class Main extends React.Component {
-    render () {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            home: false,
+            dog: false,
+            account: true
+        }
+    }
+
+
+
+    render() {
+        const { home, dog} = this.state
         return (
             <>
-                Main
+                <div>
+
+                </div>
+                {
+                    home
+                        ?
+                        <Home /> 
+                        : 
+                        dog
+                            ?
+                            <Dog />
+                            :
+                            <Account />
+                }
+
             </>
         )
     }
