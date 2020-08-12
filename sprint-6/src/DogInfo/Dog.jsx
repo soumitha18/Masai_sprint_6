6e1b5b5a927f56b8540a0395242642d489cea259
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-08-12 15:04:15 +0530

    deleted dog

--- sprint-6/src/DogInfo/Dog.jsx
+++ sprint-6/src/DogInfo/Dog.jsx
@@ -1,172 +1,172 @@
 import React from "react"
 import axios from 'axios'
-import style from './Style/dog.module.css'
+
 export default class Home extends React.Component {
-constructor(props){
-    super(props)
-    this.state ={
-        active:0,
-    data:[
-        {
-            name:"hound",
-            img:"",
-            images:[]
-        },
-        {
-            name:"airedale",
-            img:"",
-            images:[]
-        },
-        {
-            name:"boxer",
-            img:"",
-            images:[]
-        },
-        {
-            name:"affenpinscher",
-            img:"",
-            images:[]
-        },
-        {
-            name:"akita",
-            img:"",
-            images:[]
-        },
-        {
-            name:"doberman",
-            img:"",
-            images:[]
-        },
-        {
-            name:"briard",
-            img:"",
-            images:[]
-        },
-        {
-            name:"whippet",
-            img:"",
-            images:[]
-        },
-        {
-            name:"lhasa",
-            img:"",
-            images:[]
-        },
-        {
-            name:"eskimo",
-            img:"",
-            images:[]
-        },
-        {
-            name:"saluki",
-            img:"",
-            images:[]
-        },
-        {
-            name:"shiba",
-            img:"",
-            images:[]
-        },
-        {
-            name:"maltese",
-            img:"",
-            images:[]
-        },
-        {
-            name:"vizsla",
-            img:"",
-            images:[]
-        },
-        {
-            name:"african",
-            img:"",
-            images:[]
-        },
-        {
-            name:"papillon",
-            img:"",
-            images:[]
-        },
-        {
-            name:"pembroke",
-            img:"",
-            images:[]
-        },
-        {
-            name:"pitbull",
-            img:"",
-            images:[]
-        },
-        {
-            name:"redbone",
-            img:"",
-            images:[]
-        },
-        {
-            name:"chow",
-            img:"",
-            images:[]
-        },
-        {
-            name:"cairn",
-            img:"",
-            images:[]
-        },
-        {
-            name:"havanese",
-            img:"",
-            images:[]
-        },
-        {
-            name:"kelpie",
-            img:"",
-            images:[]
-        },
-        {
-            name:"labrador",
-            img:"",
-            images:[]
-        },
-    
-    ]
+    constructor(props) {
+        super(props)
+        this.state = {
+            active: 0,
+            data: [
+                {
+                    name: "hound",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "airedale",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "boxer",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "affenpinscher",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "akita",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "doberman",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "briard",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "whippet",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "lhasa",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "eskimo",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "saluki",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "shiba",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "maltese",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "vizsla",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "african",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "papillon",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "pembroke",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "pitbull",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "redbone",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "chow",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "cairn",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "havanese",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "kelpie",
+                    img: "",
+                    images: []
+                },
+                {
+                    name: "labrador",
+                    img: "",
+                    images: []
+                },
+
+            ]
+        }
+
     }
-    
-}
 
-componentDidMount(){
- for(let index=0;index<this.state.data.length;index++){
-     axios.get(`https://dog.ceo/api/breed/${this.state.data[index].name}/images/random`)
-     .then(res=>res.data)
-     .then(res=>this.setState({
-        data:this.state.data.map((item,i)=>{
-            if(i===index){
-                item.img=res.message
-                return item
+    componentDidMount() {
+        for (let index = 0; index < this.state.data.length; index++) {
+            axios.get(`https://dog.ceo/api/breed/${this.state.data[index].name}/images/random`)
+                .then(res => res.data)
+                .then(res => this.setState({
+                    data: this.state.data.map((item, i) => {
+                        if (i === index) {
+                            item.img = res.message
+                            return item
 
-            }
-            else{
-                return item
-            }
-        })
+                        }
+                        else {
+                            return item
+                        }
+                    })
 
-     },console.log(this.state)))
- }
-}
-    render (){
-  
+                }, console.log(this.state)))
+        }
+    }
+    render() {
+
         return (
             <>
-            <div className={style.div_box}>
-         {this.state.data.map(item=>( 
-                <div className={style.box}>
-                    <img src ={item.img} alt="dog name"/>
-                 
-                    <h4>{item.name} <button>more info</button></h4>
-                    
-                   
+                <div>
+                    {this.state.data.map(item => (
+                        <div>
+                            <img src={item.img} alt="dog name" />
+
+                            <h4>{item.name} <button>more info</button></h4>
+
+
+                        </div>
+                    ))}
                 </div>
-             ))}
-             </div>
             </>
         )
     }
--- sprint-6/src/DogInfo/MoreInfo.jsx
+++ sprint-6/src/DogInfo/MoreInfo.jsx
@@ -17,6 +17,7 @@ export default class MoreInfo extends React.Component{
     render () {
         return (
             <>
+                
             </>
         )
     }
