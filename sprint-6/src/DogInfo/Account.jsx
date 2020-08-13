e384dad3278b2bffd252b7997b36c8372bda08a0
Madhuri_Sonawat <madhurisonawat@gmail.com>
2020-08-13 15:45:05 +0530

    added account functionalities

--- sprint-6/src/DogInfo/Account.jsx
+++ sprint-6/src/DogInfo/Account.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import axios from "axios"
-import Payment from "./Payment"
 import data from "../dogInfo.json"
 import style from "./Style/account.module.css"
 
@@ -19,8 +18,7 @@ export default class Account extends React.Component {
             loginUser: [],
             active: this.props.dogName,
             dogImg: "",
-            dogInfo: [],
-            payment: false
+            dogInfo: []
         }
     }
 
@@ -97,22 +95,6 @@ export default class Account extends React.Component {
         })
     }
 
-    confirm = () => {
-        this.setState({
-            payment: true
-        })
-    }
-
-    finalClick = () =>{
-        alert(`Thanks for Adopting ${this.state.loginUser[0].name}. We will contact you shortly! `)
-        this.props.handle()
-    }
-
-    cancel = () =>{
-        alert("Thanks For Watching!")
-        this.props.handle()
-    }
-
     componentDidMount() {
         if (this.state.active !== "") {
             axios.get(`https://dog.ceo/api/breed/${this.state.active}/images/random`)
@@ -132,77 +114,66 @@ export default class Account extends React.Component {
         return (
             <div style={{ marginTop: "5%" }}>
                 {
-                    this.state.payment
+                    this.state.log
                         ?
-                            <Payment info={dogInfo} img={dogImg} user={loginUser} onCancel={this.cancel} onClick={this.finalClick} />
-                        :
-                        this.state.log
-                            ?
-                            <div className={style.divForm}>
-                                <img width="450px" height="250px" src="https://image.flaticon.com/icons/svg/889/889020.svg" alt="Link" />
-                                {
-                                    this.state.show
-                                        ?
-                                        <div className={style.form}>
-                                            <div className={style.formDiv}>
-                                                <input name="email" value={this.state.email} onChange={this.handleChange} type="text" placeholder="Enter the Email" />
-                                                <input name="password" value={this.state.password} placeholder="Enter the Password" onChange={this.handleChange} type="password" />
-                                                <button onClick={this.userLogin}>Login</button>
-                                            </div>
-                                            <div className={style.buttons}>
-                                                <button className={style.regis} onClick={this.handleRegister}>Register</button>
-                                            </div>
+                        <div className={style.divForm}>
+                            <img width="450px" height="250px" src="https://image.flaticon.com/icons/svg/889/889020.svg" alt="Link" />
+                            {
+                                this.state.show
+                                    ?
+                                    <div className={style.form}>
+                                        <div className={style.formDiv}>
+                                            <input name="email" value={this.state.email} onChange={this.handleChange} type="text" placeholder="Enter the Email" />
+                                            <input name="password" value={this.state.password} placeholder="Enter the Password" onChange={this.handleChange} type="password" />
+                                            <button onClick={this.userLogin}>Login</button>
                                         </div>
-                                        :
-                                        <div className={style.form}>
-                                            <div className={style.formDiv}>
-                                                <input name="name" value={this.state.name} onChange={this.handleChange} type="text" placeholder="Enter the Name" />
-                                                <input name="email" value={this.state.email} onChange={this.handleChange} type="text" placeholder="Enter the email" />
-                                                <input name="password" value={this.state.password} onChange={this.handleChange} type="password" placeholder="Enter the Password" />
-                                                <input name="password" value={this.state.password} onChange={this.handleChange} type="password" placeholder="Conform Password" />
-                                                <input name="age" value={this.state.age} onChange={this.handleChange} type="text" placeholder="Enter the Age" />
-                                                <input name="address" value={this.state.address} onChange={this.handleChange} type="text" placeholder="Enter the address" />
-                                                <button onClick={this.addUser}>Register</button>
-                                            </div>
-                                            <div className={style.buttons}>
-                                                <button className={style.login} onClick={this.handleLogin}>L<br /><br />O<br /><br />G<br /><br />I<br /><br />N</button>
-                                            </div>
+                                        <div className={style.buttons}>
+                                            <button className={style.regis} onClick={this.handleRegister}>Register</button>
                                         </div>
+                                    </div>
+                                    :
+                                    <div className={style.form}>
+                                        <div className={style.formDiv}>
+                                            <input name="name" value={this.state.name} onChange={this.handleChange} type="text" placeholder="Enter the Name" />
+                                            <input name="email" value={this.state.email} onChange={this.handleChange} type="text" placeholder="Enter the email" />
+                                            <input name="password" value={this.state.password} onChange={this.handleChange} type="password" placeholder="Enter the Password" />
+                                            <input name="password" value={this.state.password} onChange={this.handleChange} type="password" placeholder="Conform Password" />
+                                            <input name="age" value={this.state.age} onChange={this.handleChange} type="text" placeholder="Enter the Age" />
+                                            <input name="address" value={this.state.address} onChange={this.handleChange} type="text" placeholder="Enter the address" />
+                                            <button onClick={this.addUser}>Register</button>
+                                        </div>
+                                        <div className={style.buttons}>
+                                            <button className={style.login} onClick={this.handleLogin}>L<br /><br />O<br /><br />G<br /><br />I<br /><br />N</button>
+                                        </div>
+                                    </div>
 
-                                }
+                            }
+                        </div>
+                        :
+                        <div>
+                            <div className={style.fixed}>
+                                <button onClick={this.userLogout}>LogOut</button>
                             </div>
-                            :
+                            <div className={style.userCard}>
+                                <div>
+                                    <img className={style.profile} src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png" alt="User" />
+                                </div>
+                                <div>
+                                    <h2>{loginUser[0].name} <span>({loginUser[0].age} years)</span></h2>
+                                    <h3>{loginUser[0].email}</h3>
+                                    <h4>Address : {loginUser[0].address}</h4>
+                                </div>
+                            </div>
+                            <hr />
                             <div>
-                                <div className={style.fixed}>
-                                    <button onClick={this.userLogout}>LogOut</button>
+                                <div>
+                                    <img />
                                 </div>
-                                <div className={style.userCard}>
-                                    <div>
-                                        <img className={style.profile} src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png" alt="User" />
-                                    </div>
-                                    <div>
-                                        <h2>{loginUser[0].name} <span>({loginUser[0].age} years)</span></h2>
-                                        <h3>{loginUser[0].email}</h3>
-                                        <h4>Address : {loginUser[0].address}</h4>
-                                    </div>
-                                </div>
-                                <hr className={style.hr} />
-                                <h2 className={style.text}>Adopted Dog Details</h2>
-                                <div className={style.flex}>
-                                    <div>
-                                        <img className={style.dogImg} width="400px" height="400px" src={dogImg} alt="DogImage" />
-                                    </div>
-                                    <div className={style.dogInfo}>
-                                        <h2>{dogInfo[0].Name} ({dogInfo[0].Speed})</h2>
-                                        <small>{dogInfo[0].Temperament}</small>
-                                        <p><span>Colors Available :</span> {dogInfo[0].Colors} </p>
-                                        <p><span>LifeSpan : </span>{dogInfo[0].Lifespan}</p>
-                                        <p>{dogInfo[0].Description}</p>
-                                        <span>Prize : </span><em>₹{dogInfo[0].prize}</em> only
-                                            <button onClick={this.confirm}>Confirm</button>
-                                    </div>
+                                <div>
+                                    
                                 </div>
                             </div>
+                        </div>
                 }
             </div>
         )
