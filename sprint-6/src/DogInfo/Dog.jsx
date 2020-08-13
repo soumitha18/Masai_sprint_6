8fae08b020f24639eb527dc31ef5807db843a116
Madhuri_Sonawat <madhurisonawat@gmail.com>
2020-08-13 15:54:39 +0530

    added some functionalities

--- sprint-6/src/DogInfo/Dog.jsx
+++ sprint-6/src/DogInfo/Dog.jsx
@@ -1,3 +1,4 @@
+ 
 import React from "react"
 import axios from 'axios'
 import MoreInfo from "./MoreInfo"
@@ -14,145 +15,145 @@ export default class Home extends React.Component {
                     Name:"HOUND",
                     name: "hound",
                     img: "",
-                    images: []
+                   
                 },
                 {
                     Name : "AIREDALE",
                     name: "airedale",
                     img: "",
-                    images: []
+                  
                 },
                 {
                     Name : "BOXER",
                     name: "boxer",
                     img: "",
-                    images: []
+                   
                 },
                 {
                     Name : "AFFENPINSCHER",
                     name: "affenpinscher",
                     img: "",
-                    images: []
+                   
                 },
                 {
                     Name : "AKITA",
                     name: "akita",
                     img: "",
-                    images: []
+                   
                 },
                 {
                     Name: "DOBERMAN",
                     name: "doberman",
                     img: "",
-                    images: []
+                    
                 },
                 {
                     Name : "BRIARD",
                     name: "briard",
                     img: "",
-                    images: []
+                
                 },
                 {
                     Name : "WHIPPET",
                     name: "whippet",
                     img: "",
-                    images: []
+                 
                 },
                 {
                     Name : "LHASA",
                     name: "lhasa",
                     img: "",
-                    images: []
+                   
                 },
                 {
                     Name : "ESKIMO",
                     name: "eskimo",
                     img: "",
-                    images: []
+                   
                 },
                 {
                     Name : "SALUKI",
                     name: "saluki",
                     img: "",
-                    images: []
+                   
                 },
                 {
                     Name : "DINGO",
                     name: "dingo",
                     img: "",
-                    images: []
+             
                 },
                 {
                     Name : "MALTESE",
                     name: "maltese",
                     img: "",
-                    images: []
+                   
                 },
                 {
                     Name : "VIZSLA",
                     name: "vizsla",
                     img: "",
-                    images: []
+             
                 },
                 {
                     Name : "AFRICAN",
                     name: "african",
                     img: "",
-                    images: []
+                
                 },
                 {
                     Name : "PAPILLON",
                     name: "papillon",
                     img: "",
-                    images: []
+              
                 },
                 {
                     Name : "PEMBROKE",
                     name: "pembroke",
                     img: "",
-                    images: []
+                  
                 },
                 {
                     Name : "PITBULL",
                     name: "pitbull",
                     img: "",
-                    images: []
+                  
                 },
                 {
                     Name : "REDBONE",
                     name: "redbone",
                     img: "",
-                    images: []
+                  
                 },
                 {
                     Name : "CHOW",
                     name: "chow",
                     img: "",
-                    images: []
+                  
                 },
                 {
                     Name : "CAIRN",
                     name: "cairn",
                     img: "",
-                    images: []
+                  
                 },
                 {
                     Name : "HAVANESE",
                     name: "havanese",
                     img: "",
-                    images: []
+                  
                 },
                 {
                     Name : "KELPIE",
                     name: "kelpie",
                     img: "",
-                    images: []
+                  
                 },
                 {
                     Name : "LABRADOR",
                     name: "labrador",
                     img: "",
-                    images: []
+                  
                 },
             ]
         }
@@ -221,7 +222,6 @@ export default class Home extends React.Component {
                             <MoreInfo id={this.state.active} handle={this.handleUser} name={this.state.active} onClick={this.toggleStageTrue}/>
                         </div>
                 }
-
             </>
         )
     }
